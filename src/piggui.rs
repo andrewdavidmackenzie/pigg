8f12254ecd603898b4331e89f66c9afa91714e03
Your Name here only <krishnsundaram@gmail.com>
2024-05-13 17:10:01 +0530

    adding basic sandbox implementation

--- src/piggui.rs
+++ src/piggui.rs
@@ -1,5 +1,3 @@
-
-
 /// When built with the "rppal" feature for interacting with GPIO - can only be built for RPi
 #[cfg(feature = "rppal")]
 use rppal;
@@ -7,7 +5,34 @@ use rppal;
 /// When built with the "iced" feature for GUI. This can be on Linux, Macos or RPi (linux)
 #[cfg(feature = "iced")]
 use iced;
+use iced::{Element, Sandbox, Settings};
+use iced::widget::text;
+use iced::border;
 
-fn main() {
-    println!("OINK");
+fn main() -> iced::Result {
+    Gpio::run(Settings::default())
+}
+
+struct Gpio;
+
+#[derive(Debug)]
+enum Message {}
+
+impl Sandbox for Gpio {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("Pigg")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {}
+    }
+    fn view(&self) -> Element<'_, Message> {
+        text("Hello iced").into()
+    }
 }
