c38ad51cf6055ba7ce6c23aa3bb9151555aa84cc
Your Name here only <krishnsundaram@gmail.com>
2024-05-14 19:26:16 +0530

    refactoring and adding fake message enum functionality

--- src/piggui.rs
+++ src/piggui.rs
@@ -3,8 +3,8 @@ mod gpio;
 use gpio::PinConfig;
 // This binary will only be built with the "iced" feature enabled, by use of "required-features"
 // in Cargo.toml so no need for the feature to be used here for conditional compiling
-use iced::widget::{checkbox, column, container, row, text, Column, Radio, Row};
-use iced::{window, Element, Length, Sandbox, Settings};
+use iced::widget::{button, checkbox, column, container, row, text, Column, Radio, Row, Text};
+use iced::{window, Element, Length, Sandbox, Settings, alignment};
 
 fn main() -> iced::Result {
     let window = window::Settings {
@@ -25,11 +25,14 @@ fn main() -> iced::Result {
 
 struct Gpio {
     pins: [Option<PinConfig>; 40],
+    clicked: bool,
 }
 
 #[derive(Debug, Clone, Copy)]
 
-enum Message {}
+enum Message {
+    Activate,
+}
 
 impl Sandbox for Gpio {
     type Message = Message;
@@ -38,6 +41,7 @@ impl Sandbox for Gpio {
         let config = gpio::GPIOConfig::new();
         Self {
             pins: config.pin_configs,
+            clicked: false,
         }
     }
 
@@ -46,31 +50,46 @@ impl Sandbox for Gpio {
     }
 
     fn update(&mut self, message: Message) {
-        match message {}
+        match message {
+            Message::Activate => self.clicked = true,
+        }
     }
 
     fn view(&self) -> iced::Element<Self::Message> {
-        let mut column = Column::new()
-            .spacing(20)
-            .align_items(iced::Alignment::Center)
-            .width(Length::Fill);
-
-        for i in (0..self.pins.len()).step_by(2) {
-            if i + 1 < self.pins.len() {
-                let pin1_str = format!("{:?}", self.pins[i]);
-                let pin2_str = format!("{:?}", self.pins[i + 1]);
-
-                //TODO: Make this radio button
-                let row = row!(text(pin1_str), text(pin2_str)).spacing(10);
-                column = column.push(row);
-            }
-        }
-        container(column)
+        container(pin_view(&self.pins))
             .height(Length::Fill)
             .width(Length::Fill)
+            .align_x(alignment::Horizontal::Center)
+            .align_y(alignment::Vertical::Center)
             .into()
     }
+
+    fn scale_factor(&self) -> f64 {
+        0.75
+    }
+
     fn theme(&self) -> iced::Theme {
         iced::Theme::Dark
     }
+}
+
+fn pin_view(pins: &[Option<PinConfig>; 40]) -> Element<'static, Message> {
+    let mut column = Column::new()
+        .spacing(20)
+        .align_items(iced::Alignment::Center)
+        .width(Length::Fill)
+        .height(Length::Fill);
+
+    for i in 0..pins.len() / 2 {
+        
+        let row = row!(
+
+            // add radio button
+            button(Text::new("pin1")).on_press(Message::Activate),
+            button(Text::new("pin2")).on_press(Message::Activate)
+        )
+        .spacing(10);
+        column = column.push(row);
+    }
+    container(column).height(2000).width(2000).into()
 }
