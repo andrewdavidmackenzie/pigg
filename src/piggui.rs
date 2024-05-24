fc6c5bcf1658ba8af3f71ac1ca710be36b56ff17
Your Name here only <krishnsundaram@gmail.com>
2024-05-24 19:55:43 +0530

    increasing window height

--- src/piggui.rs
+++ src/piggui.rs
@@ -1,4 +1,4 @@
-use iced::{Sandbox, Settings, window};
+use iced::{window, Sandbox, Settings};
 
 mod gpio;
 mod hw;
@@ -15,6 +15,7 @@ fn main() -> Result<(), iced::Error> {
     let window = window::Settings {
         resizable: false,
         decorations: true,
+        size: iced::Size::new(800.0, 1000.0),
         ..Default::default()
     };
 
