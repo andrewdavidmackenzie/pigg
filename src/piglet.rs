dda2a21252f080a4a50e114ff535c47e4dad4ba0
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-06-03 12:54:56 +0200

    Improve piglet output

--- src/piglet.rs
+++ src/piglet.rs
@@ -4,10 +4,13 @@ mod gpio;
 #[allow(dead_code)]
 mod hw;
 
+/// Piglet will expose the same functionality from the GPIO Hardware Backend used by the GUI
+/// in Piggy, but without any GUI or related dependencies, loading a config from file and
+/// over the network.
 fn main() {
     let mut hw = hw::get();
+    println!("{}", hw.descriptor().unwrap());
     println!("Pin Descriptions: {:?}", hw.pin_descriptions());
-    println!("Hardware: {:?}", hw.descriptor());
 
     // When we write piglet for real - this will probably be sent over the network from a piggui
     let config = gpio::GPIOConfig::default();
