d495c2c0c7d41dfc01fcdb6150bc7371172e4d8e
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-06-18 23:05:15 +0200

    Rebased

--- src/views/main_row.rs
+++ src/views/main_row.rs
@@ -1,8 +1,6 @@
-use crate::pin_layout::{bcm_pin_layout_view, board_pin_layout_view};
 use crate::views::configuration_column;
-use crate::views::layout_selector::Layout;
 use crate::{Message, Piggui};
-use iced::widget::{Column, container, Row};
+use iced::widget::{container, Column, Row};
 use iced::{Alignment, Element, Length};
 
 /// Construct the view that represents the main row of the app
@@ -19,16 +17,16 @@ pub fn view(app: &Piggui) -> Element<Message> {
     );
 
     main_row = main_row.push(
-            Column::new()
-                .push(
-                    app.hardware_view
-                        .view(app.layout_selector.get())
-                        .map(Message::Hardware),
-                )
-                .align_items(Alignment::Center)
-                .height(Length::Fill)
-                .width(Length::Fill),
-        );
+        Column::new()
+            .push(
+                app.hardware_view
+                    .view(app.layout_selector.get())
+                    .map(Message::Hardware),
+            )
+            .align_items(Alignment::Center)
+            .height(Length::Fill)
+            .width(Length::Fill),
+    );
 
     container(main_row).padding([10.0, 10.0, 0.0, 10.0]).into()
 }
