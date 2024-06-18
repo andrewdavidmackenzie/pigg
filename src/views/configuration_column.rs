4433ad33fe83ca73087dc8352c79daff8e64baf5
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-06-18 12:36:20 +0200

    Make more readable and remove unneeded Row

--- src/views/configuration_column.rs
+++ src/views/configuration_column.rs
@@ -1,15 +1,10 @@
 use crate::custom_widgets::button_style::ButtonStyle;
 use crate::{Message, Piggui, ToastMessage};
-use iced::widget::{Button, Column, Row, Text};
-use iced::{Alignment, Color, Element};
+use iced::widget::{Button, Column, Text};
+use iced::{Alignment, Color, Element, Length};
 
 /// Construct the view that represents the configuration column
 pub fn view(app: &Piggui) -> Element<'static, Message> {
-    let layout_row = Row::new()
-        .push(app.layout_selector.view())
-        .align_items(Alignment::Start)
-        .spacing(10);
-
     let file_button_style = ButtonStyle {
         bg_color: Color::new(0.0, 1.0, 1.0, 1.0),
         text_color: Color::BLACK,
@@ -17,25 +12,29 @@ pub fn view(app: &Piggui) -> Element<'static, Message> {
         hovered_text_color: Color::WHITE,
         border_radius: 2.0,
     };
-    let mut configuration_column = Column::new().align_items(Alignment::Start).spacing(10);
-    configuration_column = configuration_column.push(layout_row);
-    configuration_column = configuration_column.push(
-        Button::new(Text::new("Save Configuration"))
-            .style(file_button_style.get_button_style())
-            .on_press(Message::Save),
-    );
-    configuration_column = configuration_column.push(
-        Button::new(Text::new("Load Configuration"))
-            .style(file_button_style.get_button_style())
-            .on_press(if !app.show_toast {
-                // Add a new toast if `show_toast` is false
-                Message::Load
-            } else {
-                // Close the existing toast if `show_toast` is true
-                let index = app.toasts.len() - 1;
-                Message::Toast(ToastMessage::Close(index))
-            }),
-    );
+
+    let save_button = Button::new(Text::new("Save Configuration"))
+        .style(file_button_style.get_button_style())
+        .on_press(Message::Save);
+    // TODO I would factor out the if in on_press and put it under Load message (call it"LoadRequested"?)
+    let load_button = Button::new(Text::new("Load Configuration"))
+        .style(file_button_style.get_button_style())
+        .on_press(if !app.show_toast {
+            // Add a new toast if `show_toast` is false
+            Message::Load
+        } else {
+            // Close the existing toast if `show_toast` is true
+            let index = app.toasts.len() - 1;
+            Message::Toast(ToastMessage::Close(index))
+        });
+
+    let mut configuration_column = Column::new()
+        .align_items(Alignment::Start)
+        .spacing(10)
+        .width(Length::Shrink);
+    configuration_column = configuration_column.push(app.layout_selector.view());
+    configuration_column = configuration_column.push(save_button);
+    configuration_column = configuration_column.push(load_button);
 
     configuration_column.into()
 }
