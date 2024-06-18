ee64de1f212506a2c2c55a3329c3a2afbc572c16
Your Name here only <krishnsundaram@gmail.com>
2024-06-18 18:40:00 +0530

    Implement horizontal rule for status bar

--- src/piggui.rs
+++ src/piggui.rs
@@ -382,6 +382,7 @@ impl Application for Piggui {
     fn view(&self) -> Element<Self::Message> {
         let main_col = Column::new()
             .push(main_row::view(self))
+            .push(iced::widget::horizontal_rule(10))
             .push(info_row::view(self));
 
         let content = container(main_col)
