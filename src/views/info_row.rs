daadd861df5f1f704909e5d7b8869d2b4c5ecd6d
sundaram123krishnan <krishnsundaram@gmail.com>
2024-07-08 17:50:06 +0530

    Implement hardware menu

--- src/views/info_row.rs
+++ src/views/info_row.rs
@@ -1,11 +1,15 @@
 use crate::styles::background::SetAppearance;
+use crate::styles::button_style::ButtonStyle;
 use crate::views::hardware_view::HardwareView;
 use crate::views::message_row::{MessageRow, MessageRowMessage};
 use crate::views::version::version_button;
 use crate::views::{hardware_button, unsaved_status};
 use crate::Message;
-use iced::widget::{container, Row};
+use iced::widget::{container, Button, Column, Row, Text};
 use iced::{Color, Command, Element, Length};
+use iced_aw::menu;
+use iced_aw::menu::{Item, Menu, MenuBar};
+use iced_aw::menu_bar;
 use iced_futures::Subscription;
 
 pub struct InfoRow {
@@ -31,10 +35,38 @@ impl InfoRow {
         unsaved_changes: bool,
         hardware_view: &'a HardwareView,
     ) -> Element<'a, Message> {
+        let about_button_style = ButtonStyle {
+            bg_color: Color::TRANSPARENT,
+            text_color: Color::WHITE,
+            hovered_bg_color: Color::TRANSPARENT,
+            hovered_text_color: Color::WHITE,
+            border_radius: 4.0,
+        };
+
+        let mb = menu_bar!((
+            Button::new(Text::new("Hardware")).style(about_button_style.get_button_style()),
+            {
+                menu!((Button::new(Text::new("Use local Pi Hardware"))
+                    .style(about_button_style.get_button_style())
+                    .width(Length::Fill))(
+                    Button::new(Text::new("Connect to remote Pi"))
+                        .style(about_button_style.get_button_style())
+                        .width(Length::Fill)
+                )(
+                    Button::new(Text::new("Search for Pi's on local network"))
+                        .style(about_button_style.get_button_style())
+                        .width(Length::Fill)
+                )(hardware_button::view(hardware_view)))
+                .max_width(180.0)
+                .spacing(2.0)
+                .offset(10.0)
+            }
+        ));
+
         container(
             Row::new()
                 .push(version_button())
-                .push(hardware_button::view(hardware_view))
+                .push(mb)
                 .push(unsaved_status::view(unsaved_changes))
                 .push(iced::widget::Space::with_width(Length::Fill)) // This takes up remaining space
                 .push(self.message_row.view().map(Message::InfoRow))
