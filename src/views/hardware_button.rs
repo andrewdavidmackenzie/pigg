8bc73e4967ed5550a9fe6a9f96305325c652e5af
sundaram123krishnan <krishnsundaram@gmail.com>
2024-07-09 16:29:22 +0530

    Change hardware button title

--- src/views/hardware_button.rs
+++ src/views/hardware_button.rs
@@ -2,11 +2,10 @@ use iced::widget::{Button, Text};
 use iced::{Color, Element, Length};
 
 use crate::styles::button_style::ButtonStyle;
-use crate::views::hardware_view::HardwareView;
 use crate::{Message, ToastMessage};
 
 /// Create the view that represents the clickable button that shows what hardware is connected
-pub fn view(hardware_view: &HardwareView) -> Element<Message> {
+pub fn view() -> Element<'static, Message> {
     let about_button_style = ButtonStyle {
         bg_color: Color::TRANSPARENT,
         text_color: Color::new(0.7, 0.7, 0.7, 1.0),
@@ -14,7 +13,7 @@ pub fn view(hardware_view: &HardwareView) -> Element<Message> {
         hovered_text_color: Color::WHITE,
         border_radius: 4.0,
     };
-    Button::new(Text::new(hardware_view.hw_model()))
+    Button::new(Text::new("Show Hardware Details"))
         .on_press(Message::Toast(ToastMessage::HardwareDetailsToast))
         .width(Length::Fill)
         .style(about_button_style.get_button_style())
