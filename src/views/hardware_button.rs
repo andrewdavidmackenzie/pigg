156e241607f6d78ff1642a32da93f62c993929d9
sundaram123krishnan <krishnsundaram@gmail.com>
2024-07-10 01:08:50 +0530

    Fix button appearance

--- src/views/hardware_button.rs
+++ src/views/hardware_button.rs
@@ -6,16 +6,17 @@ use crate::{Message, ToastMessage};
 
 /// Create the view that represents the clickable button that shows what hardware is connected
 pub fn view() -> Element<'static, Message> {
-    let about_button_style = ButtonStyle {
+    let hardware_button = ButtonStyle {
         bg_color: Color::TRANSPARENT,
-        text_color: Color::new(0.7, 0.7, 0.7, 1.0),
+        text_color: Color::WHITE,
         hovered_bg_color: Color::TRANSPARENT,
         hovered_text_color: Color::WHITE,
         border_radius: 4.0,
     };
+
     Button::new(Text::new("Show Hardware Details"))
         .on_press(Message::Toast(ToastMessage::HardwareDetailsToast))
         .width(Length::Fill)
-        .style(about_button_style.get_button_style())
+        .style(hardware_button.get_button_style())
         .into()
 }
