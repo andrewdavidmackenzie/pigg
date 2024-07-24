44d7fd49e2673c9d256dc5e7de20def164c12418
sundaram123krishnan <krishnsundaram@gmail.com>
2024-07-24 18:24:54 +0530

    Test modal

--- src/piggui.rs
+++ src/piggui.rs
@@ -1,11 +1,10 @@
 use clap::{Arg, ArgMatches};
 use std::env;
-
-use iced::widget::{container, Column};
+use iced::widget::{container, Column, column, text_input, text, Button, Text};
 use iced::{
-    executor, window, Application, Command, Element, Length, Settings, Subscription, Theme,
+    executor, window, Application, Command, Element, Event, Length, Settings, Subscription, Theme,
 };
-
+use modal::Modal;
 use crate::file_helper::{maybe_load_no_picker, pick_and_load, save};
 use crate::hw::config::HardwareConfig;
 use crate::toast_handler::{ToastHandler, ToastMessage};
@@ -83,6 +82,10 @@ pub enum Message {
     WindowEvent(iced::Event),
     HardwareLost,
     MenuBarButtonClicked,
+    ShowModal,
+    HideModal,
+    Submit,
+    ConnectionId(String)
 }
 
 /// [Piggui] Is the struct that holds application state and implements [Application] for Iced
@@ -93,10 +96,17 @@ pub struct Piggui {
     info_row: InfoRow,
     toast_handler: ToastHandler,
     hardware_view: HardwareView,
+    show_modal: bool,
+    connection_id: String,
 }
 
 async fn empty() {}
-
+impl Piggui {
+    fn hide_modal(&mut self) {
+        self.show_modal = false;
+        self.connection_id.clear();
+    }
+}
 impl Application for Piggui {
     type Executor = executor::Default;
     type Message = Message;
@@ -122,6 +132,8 @@ impl Application for Piggui {
                 info_row: InfoRow::new(),
                 toast_handler: ToastHandler::new(),
                 hardware_view: HardwareView::new(nodeid),
+                show_modal: false,
+                connection_id: String::new(),
             },
             maybe_load_no_picker(config_filename),
         )
@@ -147,6 +159,27 @@ impl Application for Piggui {
                         return window::close(window::Id::MAIN);
                     }
                 }
+            }
+
+            ConnectionId(connection_id) => {
+                self.connection_id = connection_id;
+                return Command::none();
+            }
+            Submit => {
+                if !self.connection_id.is_empty() {
+                    self.hide_modal();
+                }
+
+                return Command::none();
+            }
+
+            ShowModal => {
+                self.show_modal = true;
+                return iced::widget::focus_next();
+            }
+            HideModal => {
+                self.hide_modal();
+                return Command::none();
             }
 
             MenuBarButtonClicked => {
@@ -238,7 +271,37 @@ impl Application for Piggui {
             .center_x()
             .center_y();
 
-        self.toast_handler.view(content.into())
+        if self.show_modal {
+
+            let modal = container(
+                column![
+                    text("Connect to Remote Pi").size(24),
+                    column![
+                        column![
+                            text("Connection id").size(12),
+                            text_input("Enter 10 digits connection id", &self.connection_id,)
+                                .on_input(Message::ConnectionId)
+                                .on_submit(Message::Submit)
+                                .padding(5),
+                        ]
+                        .spacing(5),
+                        Button::new(Text::new("Connect")).on_press(Message::HideModal)
+                    ]
+                    .spacing(10)
+                ]
+                    .spacing(20),
+            )
+                .width(300)
+                .padding(10);
+
+            Modal::new(content, modal)
+                .on_blur(Message::HideModal)
+                .into()
+        } else {
+
+            self.toast_handler.view(content.into())
+        }
+
     }
 
     fn theme(&self) -> Theme {
@@ -302,5 +365,328 @@ mod tests {
             toast.body,
             "You have unsaved changes. Do you want to continue without saving?"
         );
+    }
+}
+mod modal {
+    use iced::advanced::layout::{self, Layout};
+    use iced::advanced::overlay;
+    use iced::advanced::renderer;
+    use iced::advanced::widget::{self, Widget};
+    use iced::advanced::{self, Clipboard, Shell};
+    use iced::alignment::Alignment;
+    use iced::event;
+    use iced::mouse;
+    use iced::{Color, Element, Event, Length, Point, Rectangle, Size, Vector};
+
+    /// A widget that centers a modal element over some base element
+    pub struct Modal<'a, Message, Theme, Renderer> {
+        base: Element<'a, Message, Theme, Renderer>,
+        modal: Element<'a, Message, Theme, Renderer>,
+        on_blur: Option<Message>,
+    }
+
+    impl<'a, Message, Theme, Renderer> Modal<'a, Message, Theme, Renderer> {
+        /// Returns a new [`Modal`]
+        pub fn new(
+            base: impl Into<Element<'a, Message, Theme, Renderer>>,
+            modal: impl Into<Element<'a, Message, Theme, Renderer>>,
+        ) -> Self {
+            Self {
+                base: base.into(),
+                modal: modal.into(),
+                on_blur: None,
+            }
+        }
+
+        /// Sets the message that will be produces when the background
+        /// of the [`Modal`] is pressed
+        pub fn on_blur(self, on_blur: Message) -> Self {
+            Self {
+                on_blur: Some(on_blur),
+                ..self
+            }
+        }
+    }
+
+    impl<'a, Message, Theme, Renderer> Widget<Message, Theme, Renderer>
+    for Modal<'a, Message, Theme, Renderer>
+    where
+        Renderer: advanced::Renderer,
+        Message: Clone,
+    {
+        fn children(&self) -> Vec<widget::Tree> {
+            vec![
+                widget::Tree::new(&self.base),
+                widget::Tree::new(&self.modal),
+            ]
+        }
+
+        fn diff(&self, tree: &mut widget::Tree) {
+            tree.diff_children(&[&self.base, &self.modal]);
+        }
+
+        fn size(&self) -> Size<Length> {
+            self.base.as_widget().size()
+        }
+
+        fn layout(
+            &self,
+            tree: &mut widget::Tree,
+            renderer: &Renderer,
+            limits: &layout::Limits,
+        ) -> layout::Node {
+            self.base.as_widget().layout(
+                &mut tree.children[0],
+                renderer,
+                limits,
+            )
+        }
+
+        fn on_event(
+            &mut self,
+            state: &mut widget::Tree,
+            event: Event,
+            layout: Layout<'_>,
+            cursor: mouse::Cursor,
+            renderer: &Renderer,
+            clipboard: &mut dyn Clipboard,
+            shell: &mut Shell<'_, Message>,
+            viewport: &Rectangle,
+        ) -> event::Status {
+            self.base.as_widget_mut().on_event(
+                &mut state.children[0],
+                event,
+                layout,
+                cursor,
+                renderer,
+                clipboard,
+                shell,
+                viewport,
+            )
+        }
+
+        fn draw(
+            &self,
+            state: &widget::Tree,
+            renderer: &mut Renderer,
+            theme: &Theme,
+            style: &renderer::Style,
+            layout: Layout<'_>,
+            cursor: mouse::Cursor,
+            viewport: &Rectangle,
+        ) {
+            self.base.as_widget().draw(
+                &state.children[0],
+                renderer,
+                theme,
+                style,
+                layout,
+                cursor,
+                viewport,
+            );
+        }
+
+        fn overlay<'b>(
+            &'b mut self,
+            state: &'b mut widget::Tree,
+            layout: Layout<'_>,
+            _renderer: &Renderer,
+            translation: Vector,
+        ) -> Option<overlay::Element<'b, Message, Theme, Renderer>> {
+            Some(overlay::Element::new(Box::new(Overlay {
+                position: layout.position() + translation,
+                content: &mut self.modal,
+                tree: &mut state.children[1],
+                size: layout.bounds().size(),
+                on_blur: self.on_blur.clone(),
+            })))
+        }
+
+        fn mouse_interaction(
+            &self,
+            state: &widget::Tree,
+            layout: Layout<'_>,
+            cursor: mouse::Cursor,
+            viewport: &Rectangle,
+            renderer: &Renderer,
+        ) -> mouse::Interaction {
+            self.base.as_widget().mouse_interaction(
+                &state.children[0],
+                layout,
+                cursor,
+                viewport,
+                renderer,
+            )
+        }
+
+        fn operate(
+            &self,
+            state: &mut widget::Tree,
+            layout: Layout<'_>,
+            renderer: &Renderer,
+            operation: &mut dyn widget::Operation<Message>,
+        ) {
+            self.base.as_widget().operate(
+                &mut state.children[0],
+                layout,
+                renderer,
+                operation,
+            );
+        }
+    }
+
+    struct Overlay<'a, 'b, Message, Theme, Renderer> {
+        position: Point,
+        content: &'b mut Element<'a, Message, Theme, Renderer>,
+        tree: &'b mut widget::Tree,
+        size: Size,
+        on_blur: Option<Message>,
+    }
+
+    impl<'a, 'b, Message, Theme, Renderer>
+    overlay::Overlay<Message, Theme, Renderer>
+    for Overlay<'a, 'b, Message, Theme, Renderer>
+    where
+        Renderer: advanced::Renderer,
+        Message: Clone,
+    {
+        fn layout(
+            &mut self,
+            renderer: &Renderer,
+            _bounds: Size,
+        ) -> layout::Node {
+            let limits = layout::Limits::new(Size::ZERO, self.size)
+                .width(Length::Fill)
+                .height(Length::Fill);
+
+            let child = self
+                .content
+                .as_widget()
+                .layout(self.tree, renderer, &limits)
+                .align(Alignment::Center, Alignment::Center, limits.max());
+
+            layout::Node::with_children(self.size, vec![child])
+                .move_to(self.position)
+        }
+
+        fn on_event(
+            &mut self,
+            event: Event,
+            layout: Layout<'_>,
+            cursor: mouse::Cursor,
+            renderer: &Renderer,
+            clipboard: &mut dyn Clipboard,
+            shell: &mut Shell<'_, Message>,
+        ) -> event::Status {
+            let content_bounds = layout.children().next().unwrap().bounds();
+
+            if let Some(message) = self.on_blur.as_ref() {
+                if let Event::Mouse(mouse::Event::ButtonPressed(
+                                        mouse::Button::Left,
+                                    )) = &event
+                {
+                    if !cursor.is_over(content_bounds) {
+                        shell.publish(message.clone());
+                        return event::Status::Captured;
+                    }
+                }
+            }
+
+            self.content.as_widget_mut().on_event(
+                self.tree,
+                event,
+                layout.children().next().unwrap(),
+                cursor,
+                renderer,
+                clipboard,
+                shell,
+                &layout.bounds(),
+            )
+        }
+
+        fn draw(
+            &self,
+            renderer: &mut Renderer,
+            theme: &Theme,
+            style: &renderer::Style,
+            layout: Layout<'_>,
+            cursor: mouse::Cursor,
+        ) {
+            renderer.fill_quad(
+                renderer::Quad {
+                    bounds: layout.bounds(),
+                    ..renderer::Quad::default()
+                },
+                Color {
+                    a: 0.80,
+                    ..Color::BLACK
+                },
+            );
+
+            self.content.as_widget().draw(
+                self.tree,
+                renderer,
+                theme,
+                style,
+                layout.children().next().unwrap(),
+                cursor,
+                &layout.bounds(),
+            );
+        }
+
+        fn operate(
+            &mut self,
+            layout: Layout<'_>,
+            renderer: &Renderer,
+            operation: &mut dyn widget::Operation<Message>,
+        ) {
+            self.content.as_widget().operate(
+                self.tree,
+                layout.children().next().unwrap(),
+                renderer,
+                operation,
+            );
+        }
+
+        fn mouse_interaction(
+            &self,
+            layout: Layout<'_>,
+            cursor: mouse::Cursor,
+            viewport: &Rectangle,
+            renderer: &Renderer,
+        ) -> mouse::Interaction {
+            self.content.as_widget().mouse_interaction(
+                self.tree,
+                layout.children().next().unwrap(),
+                cursor,
+                viewport,
+                renderer,
+            )
+        }
+
+        fn overlay<'c>(
+            &'c mut self,
+            layout: Layout<'_>,
+            renderer: &Renderer,
+        ) -> Option<overlay::Element<'c, Message, Theme, Renderer>> {
+            self.content.as_widget_mut().overlay(
+                self.tree,
+                layout.children().next().unwrap(),
+                renderer,
+                Vector::ZERO,
+            )
+        }
+    }
+
+    impl<'a, Message, Theme, Renderer> From<Modal<'a, Message, Theme, Renderer>>
+    for Element<'a, Message, Theme, Renderer>
+    where
+        Theme: 'a,
+        Message: 'a + Clone,
+        Renderer: 'a + advanced::Renderer,
+    {
+        fn from(modal: Modal<'a, Message, Theme, Renderer>) -> Self {
+            Element::new(modal)
+        }
     }
 }
