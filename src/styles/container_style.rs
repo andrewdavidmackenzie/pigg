d7646287f6fe71a4facb2e4e933ec3c814657f40
sundaram123krishnan <krishnsundaram@gmail.com>
2024-07-28 22:22:58 +0530

    Implement more container style properties

--- src/styles/container_style.rs
+++ src/styles/container_style.rs
@@ -1,9 +1,12 @@
 
-use iced::{Border, Color, Theme};
+use iced::{Background, Border, Color, Theme};
 use iced::widget::container;
 
 pub struct ContainerStyle {
     pub border_color: Color,
+    pub background_color: Color,
+    pub border_width: f32,
+    pub border_radius: f32,
 }
 
 impl container::StyleSheet for ContainerStyle {
@@ -11,10 +14,11 @@ impl container::StyleSheet for ContainerStyle {
 
     fn appearance(&self, _style: &Self::Style) -> container::Appearance {
         container::Appearance {
+            background: Some(Background::Color(self.background_color)),
             border: Border {
                 color: self.border_color,
-                width: 2.0,
-                radius: 2.0.into(),
+                width: self.border_width,
+                radius: self.border_radius.into(),
             },
             ..Default::default()
         }
@@ -24,7 +28,10 @@ impl container::StyleSheet for ContainerStyle {
 impl ContainerStyle {
     pub fn get_container_style(&self) -> iced::widget::theme::Container {
         iced::widget::theme::Container::Custom(Box::new(ContainerStyle {
+            background_color: self.background_color,
             border_color: self.border_color,
+            border_width: self.border_width,
+            border_radius: self.border_radius,
         }))
     }
 }
