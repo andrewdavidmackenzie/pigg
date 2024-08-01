c4a0dcd0ec2e1411f2a59cdb3bbf27616875e916
sundaram123krishnan <krishnsundaram@gmail.com>
2024-08-02 01:02:03 +0530

    Avoid conditional rendering of menu items based on features

--- src/views/info_row.rs
+++ src/views/info_row.rs
@@ -1,32 +1,36 @@
-use iced::widget::{container, Row};
-use iced::{Color, Command, Element, Length};
-use iced_aw::menu;
-use iced_aw::menu::{MenuBar, StyleSheet};
-use iced_aw::style::MenuBarStyle;
-use iced_futures::core::Background;
-use iced_futures::Subscription;
-
+use crate::connect_dialog_handler::ConnectDialogMessage;
 use crate::styles::background::SetAppearance;
 use crate::styles::button_style::ButtonStyle;
 use crate::views::hardware_view::{HardwareTarget, HardwareView};
 use crate::views::message_row::{MessageMessage, MessageRow, MessageRowMessage};
+use crate::views::unsaved_status;
 use crate::views::version::version_button;
-use crate::views::{hardware_menu, unsaved_status};
-use crate::Message;
+use crate::HardwareTarget::NoHW;
+use crate::HardwareTarget::Remote;
+use crate::{Message, ToastMessage};
+use iced::widget::{container, Button, Row, Text};
+use iced::{Color, Command, Element, Length};
+use iced_aw::menu::{Item, StyleSheet};
+use iced_aw::style::MenuBarStyle;
+use iced_aw::{menu, menu_bar};
+use iced_futures::core::Background;
+use iced_futures::Subscription;
 
-pub(crate) const MENU_BAR_BUTTON_STYLE: ButtonStyle = ButtonStyle {
+const MENU_WIDTH: f32 = 200.0;
+
+const ENABLED_MENU_BUTTON_STYLE: ButtonStyle = ButtonStyle {
     bg_color: Color::TRANSPARENT,
-    text_color: Color::from_rgba(0.7, 0.7, 0.7, 1.0),
+    text_color: Color::WHITE,
     hovered_bg_color: Color::TRANSPARENT,
     hovered_text_color: Color::WHITE,
     border_radius: 4.0,
 };
 
-pub(crate) const MENU_BUTTON_STYLE: ButtonStyle = ButtonStyle {
+const DISABLED_MENU_BUTTON_STYLE: ButtonStyle = ButtonStyle {
     bg_color: Color::TRANSPARENT,
-    text_color: Color::WHITE,
-    hovered_bg_color: Color::TRANSPARENT,
-    hovered_text_color: Color::WHITE,
+    text_color: Color::from_rgb(0.5, 0.5, 0.5), // Medium grey text color
+    hovered_bg_color: Color::from_rgb(0.2, 0.2, 0.2),
+    hovered_text_color: Color::from_rgb(0.5, 0.5, 0.5),
     border_radius: 4.0,
 };
 
@@ -59,9 +63,59 @@ impl InfoRow {
         hardware_view: &'a HardwareView,
         hardware_target: &HardwareTarget,
     ) -> Element<'a, Message> {
-        let hardware_root = hardware_menu::item(hardware_view, hardware_target);
+        let menu_bar_button_style = ButtonStyle {
+            bg_color: Color::TRANSPARENT,
+            text_color: Color::new(0.7, 0.7, 0.7, 1.0),
+            hovered_bg_color: Color::TRANSPARENT,
+            hovered_text_color: Color::WHITE,
+            border_radius: 4.0,
+        };
 
-        let mb = MenuBar::new(vec![hardware_root]).style(|theme: &iced::Theme| menu::Appearance {
+        let model = match hardware_view.hw_model() {
+            None => "No Hardware connected".to_string(),
+            Some(model) => match hardware_target {
+                NoHW => "No Hardware connected".to_string(),
+                HardwareTarget::Local => format!("{}@Local", model),
+                Remote(_, _) => format!("{}@Remote", model),
+            },
+        };
+
+        let mb = menu_bar!((
+            Button::new(Text::new(model))
+                .style(menu_bar_button_style.get_button_style())
+                .on_press(Message::MenuBarButtonClicked),
+            {
+                menu!((Button::new("Disconnect from Hardware")
+                    .width(Length::Fill)
+                    .on_press(Message::ConnectRequest(NoHW))
+                    .style(ENABLED_MENU_BUTTON_STYLE.get_button_style()))(
+                    Button::new("Connect to remote Pi...")
+                        .width(Length::Fill)
+                        .on_press(Message::ConnectDialog(
+                            ConnectDialogMessage::ShowConnectDialog,
+                        ))
+                        .style(ENABLED_MENU_BUTTON_STYLE.get_button_style())
+                )(
+                    Button::new("Use local GPIO")
+                        .on_press(Message::ConnectRequest(HardwareTarget::Local))
+                        .style(ENABLED_MENU_BUTTON_STYLE.get_button_style())
+                        .width(Length::Fill)
+                )(
+                    Button::new("Search for Pi's on local network...")
+                        .width(Length::Fill)
+                        .style(DISABLED_MENU_BUTTON_STYLE.get_button_style())
+                )(
+                    Button::new(Text::new("Show Hardware Details"))
+                        .on_press(Message::Toast(ToastMessage::HardwareDetailsToast))
+                        .width(Length::Fill)
+                        .style(ENABLED_MENU_BUTTON_STYLE.get_button_style())
+                ))
+                .width(MENU_WIDTH)
+                .spacing(2.0)
+                .offset(20.0)
+            }
+        ))
+        .style(|theme: &iced::Theme| menu::Appearance {
             bar_background: Background::Color(Color::TRANSPARENT),
             menu_shadow: iced::Shadow {
                 color: Color::BLACK,
