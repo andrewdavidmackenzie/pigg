f81d50d6fd26cf224d72584ba7d830feee72b790
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-08-01 16:13:15 +0200

    Fic #cfg

--- src/hw/mod.rs
+++ src/hw/mod.rs
@@ -14,12 +14,20 @@ pub mod config;
 /// There are two implementations of [`Hardware`] trait:
 /// * fake_hw - used on host (macOS, Linux, etc.) to show and develop GUI without real HW
 /// * pi_hw - Raspberry Pi using "rppal" crate: Should support most Pi hardware from Model B
-// TODO do this by having build script detect pi and emitting a feature
 // TODO change method to just modify path to hw module fake/pi
-#[cfg(not(all(target_os = "linux", target_arch = "aarch64", target_env = "gnu")))]
+#[cfg(not(all(
+    target_os = "linux",
+    any(target_arch = "aarch64", target_arch = "arm"),
+    target_env = "gnu"
+)))]
 mod fake_hw;
-#[cfg(all(target_os = "linux", target_arch = "aarch64", target_env = "gnu"))]
+#[cfg(all(
+    target_os = "linux",
+    any(target_arch = "aarch64", target_arch = "arm"),
+    target_env = "gnu"
+))]
 mod pi_hw;
+
 pub(crate) mod pin_description;
 mod pin_descriptions;
 pub mod pin_function;
@@ -36,12 +44,20 @@ pub type PinLevel = bool;
 pub const PIGLET_ALPN: &[u8] = b"pigg/piglet/0";
 
 /// Get the implementation we will use to access the underlying hardware via the [Hardware] trait
-#[cfg(all(target_os = "linux", target_arch = "aarch64", target_env = "gnu"))]
+#[cfg(all(
+    target_os = "linux",
+    any(target_arch = "aarch64", target_arch = "arm"),
+    target_env = "gnu"
+))]
 pub fn get() -> impl Hardware {
     pi_hw::get()
 }
 
-#[cfg(not(all(target_os = "linux", target_arch = "aarch64", target_env = "gnu")))]
+#[cfg(not(all(
+    target_os = "linux",
+    any(target_arch = "aarch64", target_arch = "arm"),
+    target_env = "gnu"
+)))]
 pub fn get() -> impl Hardware {
     fake_hw::get()
 }
