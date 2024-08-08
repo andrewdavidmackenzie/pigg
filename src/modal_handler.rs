b98a331c4b0007c6fef53a36a941e55a4a23d85f
sundaram123krishnan <krishnsundaram@gmail.com>
2024-08-08 19:29:14 +0530

    Implement exit functionality of the app

--- src/modal_handler.rs
+++ src/modal_handler.rs
@@ -1,9 +1,11 @@
+use crate::connect_dialog_handler::{
+    MODAL_CANCEL_BUTTON_STYLE, MODAL_CONNECT_BUTTON_STYLE, MODAL_CONTAINER_STYLE,
+};
+use crate::styles::text_style::TextStyle;
 use crate::views::hardware_view::HardwareView;
 use crate::Message;
-use iced::widget::{button, column, container, Text, text};
-use iced::{Application, Color, Command, Element};
-use crate::connect_dialog_handler::{MODAL_CANCEL_BUTTON_STYLE, MODAL_CONTAINER_STYLE};
-use crate::styles::text_style::TextStyle;
+use iced::widget::{button, column, container, text, Row, Text};
+use iced::{window, Application, Color, Command, Element};
 
 pub struct DisplayModal {
     pub show_modal: bool,
@@ -16,17 +18,17 @@ pub struct DisplayModal {
 pub enum ModalMessage {
     ShowModal,
     HideModal,
-    UnsavedChangesModal,
     UnsavedChangesExitModal,
     HardwareDetailsModal,
     VersionModal,
+    ExitApp,
 }
 
 impl DisplayModal {
     pub fn new() -> Self {
         Self {
-            title: String::new(),
-            body: String::new(),
+            title: String::new(), // Title of the modal
+            body: String::new(), // Body of the modal
             show_modal: false,
             is_warning: false,
         }
@@ -36,49 +38,53 @@ impl DisplayModal {
         message: ModalMessage,
         hardware_view: &HardwareView,
     ) -> Command<Message> {
-        match message {
+        return match message {
             ModalMessage::ShowModal => {
                 self.show_modal = true;
-                return Command::none();
+                Command::none()
             }
             ModalMessage::HideModal => {
                 self.show_modal = false;
-                return Command::none();
-            }
-            ModalMessage::UnsavedChangesModal => {
-                self.show_modal = true;
-                self.is_warning = true;
-                self.title = "Unsaved Changes".to_string();
-                self.body = "You have unsaved changes. Do you want to exit without saving?".to_string();
-                return Command::none();
+                Command::none()
             }
 
+            // Display warning for unsaved changes
             ModalMessage::UnsavedChangesExitModal => {
                 self.show_modal = true;
                 self.is_warning = true;
                 self.title = "Unsaved Changes".to_string();
-                self.body = "You have unsaved changes. Do you want to exit without saving?".to_string();
-                return Command::none();
+                self.body =
+                    "You have unsaved changes. Do you want to exit without saving?".to_string();
+                Command::none()
             }
 
+            // Display hardware information
             ModalMessage::HardwareDetailsModal => {
                 self.show_modal = true;
                 self.is_warning = false;
                 self.title = "About Connected Hardware".to_string();
                 self.body = hardware_view.hw_description().to_string();
-                return Command::none();
+                Command::none()
             }
+
+            // Display piggui information
             ModalMessage::VersionModal => {
                 self.show_modal = true;
                 self.is_warning = false;
                 self.title = "About Piggui".to_string();
                 self.body = crate::views::version::version().to_string();
-                return Command::none();
+                Command::none()
             }
-        }
+
+            // Exits the Application
+            ModalMessage::ExitApp => {
+                window::close(window::Id::MAIN)
+            }
+        };
     }
 
     pub fn view(&self) -> Element<Message> {
+        let mut button_row = Row::new();
         let mut text_style = TextStyle {
             text_color: Color::new(0.447, 0.624, 0.812, 1.0),
         };
@@ -86,24 +92,40 @@ impl DisplayModal {
             text_style = TextStyle {
                 text_color: Color::new(0.988, 0.686, 0.243, 1.0),
             };
+            button_row = button_row.push(
+                button("Exit without saving")
+                    .on_press(Message::ModalHandle(ModalMessage::ExitApp))
+                    .style(MODAL_CANCEL_BUTTON_STYLE.get_button_style()),
+            ); // Exits the application
+            button_row = button_row
+                .push(
+                    button("Save")
+                        .on_press(Message::ModalHandle(ModalMessage::HideModal))
+                        .style(MODAL_CONNECT_BUTTON_STYLE.get_button_style()),
+                )
+                .spacing(290);
+        } else {
+            button_row = button_row.push(
+                button("Close")
+                    .on_press(Message::ModalHandle(ModalMessage::HideModal))
+                    .style(MODAL_CANCEL_BUTTON_STYLE.get_button_style()),
+            );
         }
+
         container(
             column![column![
-                    text(self.title.clone()).size(20).style(text_style.get_text_color()),
-                    column! [
-                    text(self.body.clone()).size(14),
-                ].spacing(10),
-                    column![
-                        button("Cancel").on_press(Message::ModalHandle(ModalMessage::HideModal)).style(MODAL_CANCEL_BUTTON_STYLE.get_button_style()),
-                    ]
-                    .spacing(5),
-                ]
-                .spacing(10)]
-                .spacing(20),
+                text(self.title.clone())
+                    .size(20)
+                    .style(text_style.get_text_color()),
+                column![text(self.body.clone()).size(14),].spacing(10),
+                column![button_row].spacing(5),
+            ]
+            .spacing(10)]
+            .spacing(20),
         )
-            .style(MODAL_CONTAINER_STYLE.get_container_style())
-            .width(520)
-            .padding(15)
-            .into()
+        .style(MODAL_CONTAINER_STYLE.get_container_style())
+        .width(520)
+        .padding(15)
+        .into()
     }
 }
