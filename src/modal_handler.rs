be6ddbf200f296fb285de48f8d70f5a7ef60a743
sundaram123krishnan <krishnsundaram@gmail.com>
2024-08-08 18:41:51 +0530

    Change title text color based on the modal

--- src/modal_handler.rs
+++ src/modal_handler.rs
@@ -1,12 +1,15 @@
 use crate::views::hardware_view::HardwareView;
 use crate::Message;
 use iced::widget::{button, column, container, Text, text};
-use iced::{Command, Element};
+use iced::{Application, Color, Command, Element};
+use crate::connect_dialog_handler::{MODAL_CANCEL_BUTTON_STYLE, MODAL_CONTAINER_STYLE};
+use crate::styles::text_style::TextStyle;
 
 pub struct DisplayModal {
     pub show_modal: bool,
     title: String,
     body: String,
+    is_warning: bool,
 }
 
 #[derive(Clone, Debug)]
@@ -25,6 +28,7 @@ impl DisplayModal {
             title: String::new(),
             body: String::new(),
             show_modal: false,
+            is_warning: false,
         }
     }
     pub fn update(
@@ -43,6 +47,7 @@ impl DisplayModal {
             }
             ModalMessage::UnsavedChangesModal => {
                 self.show_modal = true;
+                self.is_warning = true;
                 self.title = "Unsaved Changes".to_string();
                 self.body = "You have unsaved changes. Do you want to exit without saving?".to_string();
                 return Command::none();
@@ -50,6 +55,7 @@ impl DisplayModal {
 
             ModalMessage::UnsavedChangesExitModal => {
                 self.show_modal = true;
+                self.is_warning = true;
                 self.title = "Unsaved Changes".to_string();
                 self.body = "You have unsaved changes. Do you want to exit without saving?".to_string();
                 return Command::none();
@@ -57,13 +63,15 @@ impl DisplayModal {
 
             ModalMessage::HardwareDetailsModal => {
                 self.show_modal = true;
-                self.title = "About connected Hardware".to_string();
+                self.is_warning = false;
+                self.title = "About Connected Hardware".to_string();
                 self.body = hardware_view.hw_description().to_string();
                 return Command::none();
             }
             ModalMessage::VersionModal => {
                 self.show_modal = true;
-                self.title = "Version is".to_string();
+                self.is_warning = false;
+                self.title = "About Piggui".to_string();
                 self.body = crate::views::version::version().to_string();
                 return Command::none();
             }
@@ -71,24 +79,29 @@ impl DisplayModal {
     }
 
     pub fn view(&self) -> Element<Message> {
+        let mut text_style = TextStyle {
+            text_color: Color::new(0.447, 0.624, 0.812, 1.0),
+        };
+        if self.is_warning {
+            text_style = TextStyle {
+                text_color: Color::new(0.988, 0.686, 0.243, 1.0),
+            };
+        }
         container(
             column![column![
-                    text("Modal").size(20),
-                    column![
-                        text(self.title.clone()).size(12),
-                    ]
-                    .spacing(10),
+                    text(self.title.clone()).size(20).style(text_style.get_text_color()),
                     column! [
-                    text(self.body.clone()).size(12),
+                    text(self.body.clone()).size(14),
                 ].spacing(10),
                     column![
-                        button("Cancel").on_press(Message::ModalHandle(ModalMessage::HideModal)),
+                        button("Cancel").on_press(Message::ModalHandle(ModalMessage::HideModal)).style(MODAL_CANCEL_BUTTON_STYLE.get_button_style()),
                     ]
                     .spacing(5),
                 ]
                 .spacing(10)]
                 .spacing(20),
         )
+            .style(MODAL_CONTAINER_STYLE.get_container_style())
             .width(520)
             .padding(15)
             .into()
