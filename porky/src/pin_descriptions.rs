d7e18d7e9c015bb554bfacc49f034688b6e5658c
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-08-23 23:10:39 +0200

    Send 40 pin descriptions, and send real unique ID as serial number.

---
+++ porky/src/pin_descriptions.rs
@@ -0,0 +1,290 @@
+use crate::hw_definition::description::PinDescription;
+use crate::hw_definition::pin_function::PinFunction;
+
+const PIN_1: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_2: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_3: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_4: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_5: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_6: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_7: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_8: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_9: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_10: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_11: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_12: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_13: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_14: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_15: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_16: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_17: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_18: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_19: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_20: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_21: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_22: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_23: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_24: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_25: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_26: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_27: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_28: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_29: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_30: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_31: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_32: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_33: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_34: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_35: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_36: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_37: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_38: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_39: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+const PIN_40: PinDescription = PinDescription {
+    bpn: 1,
+    bcm: None,
+    name: "3V3",
+    options: &[PinFunction::Power3V3],
+};
+
+/// Array of 40 [PinDescription]s of the 40 pins on a Pi Pico W
+pub const PIN_DESCRIPTIONS: [PinDescription; 40] = [
+    PIN_1, PIN_2, PIN_3, PIN_4, PIN_5, PIN_6, PIN_7, PIN_8, PIN_9, PIN_10, PIN_11, PIN_12, PIN_13,
+    PIN_14, PIN_15, PIN_16, PIN_17, PIN_18, PIN_19, PIN_20, PIN_21, PIN_22, PIN_23, PIN_24, PIN_25,
+    PIN_26, PIN_27, PIN_28, PIN_29, PIN_30, PIN_31, PIN_32, PIN_33, PIN_34, PIN_35, PIN_36, PIN_37,
+    PIN_38, PIN_39, PIN_40,
+];
