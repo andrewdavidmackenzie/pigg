7c250b278709c8edd92f37180e1bf0ff44381e4c
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-11-08 22:17:45 +0100

    Logic to track USB porky finding and losing

--- src/usb_raw.rs
+++ src/usb_raw.rs
@@ -1,4 +1,5 @@
 use crate::hw_definition::description::HardwareDescription;
+use crate::usb_raw::USBState::{Connected, Disconnected};
 use crate::Message;
 use async_std::prelude::Stream;
 use futures::SinkExt;
@@ -12,12 +13,20 @@ use std::time::Duration;
 #[derive(Debug, Clone)]
 pub enum USBEvent {
     DeviceFound(HardwareDescription),
+    DeviceLost(HardwareDescription),
     Error(String),
 }
 
+pub enum USBState {
+    /// Just starting up, we have not yet set up a channel between GUI and Listener
+    Disconnected,
+    /// The subscription is ready and will listen for config events on the channel contained
+    Connected(HardwareDescription),
+}
+
 /// Try and find an attached "porky" USB device based on the vendor id and product id
-fn get_porky() -> Result<Interface, io::Error> {
-    let mut device_list = nusb::list_devices()?;
+fn get_porky() -> Option<Interface> {
+    let mut device_list = nusb::list_devices().ok()?;
     let porky_info = device_list
         .find(|d| d.vendor_id() == 0xbabe && d.product_id() == 0xface)
         .ok_or("No Porky Found")
@@ -26,9 +35,10 @@ fn get_porky() -> Result<Interface, io::Error> {
                 io::ErrorKind::NotFound,
                 "Could not find at attached porky device",
             )
-        })?;
-    let device = porky_info.open()?;
-    device.claim_interface(0)
+        })
+        .ok()?;
+    let device = porky_info.open().ok()?;
+    device.claim_interface(0).ok()
 }
 
 async fn get_hardware_description(porky: &Interface) -> Result<HardwareDescription, String> {
@@ -68,23 +78,37 @@ async fn get_hardware_description(porky: &Interface) -> Result<HardwareDescripti
 
 /// A stream of [USBEvent] to a possibly connected porky
 pub fn subscribe() -> impl Stream<Item = USBEvent> {
+    let mut usb_state = Disconnected;
+
     stream::channel(100, move |gui_sender| async move {
         loop {
             let mut gui_sender_clone = gui_sender.clone();
             tokio::time::sleep(Duration::from_secs(1)).await;
 
-            if let Ok(porky) = get_porky() {
-                match get_hardware_description(&porky).await {
+            let porky_found = get_porky();
+
+            usb_state = match (porky_found, &usb_state) {
+                (Some(porky), Disconnected) => match get_hardware_description(&porky).await {
                     Ok(hardware_description) => {
                         let _ = gui_sender_clone
-                            .send(USBEvent::DeviceFound(hardware_description))
+                            .send(USBEvent::DeviceFound(hardware_description.clone()))
                             .await;
+                        Connected(hardware_description)
                     }
                     Err(e) => {
                         let _ = gui_sender_clone.send(USBEvent::Error(e)).await;
+                        Disconnected
                     }
+                },
+                (Some(_), Connected(hw)) => Connected(hw.clone()),
+                (None, Disconnected) => Disconnected,
+                (None, Connected(hardware_description)) => {
+                    let _ = gui_sender_clone
+                        .send(USBEvent::DeviceLost(hardware_description.clone()))
+                        .await;
+                    Disconnected
                 }
-            }
+            };
         }
     })
 }
@@ -95,6 +119,10 @@ pub fn usb_event(event: USBEvent) -> Task<Message> {
     match event {
         USBEvent::DeviceFound(hardware_description) => {
             println!("USB Device Found: {}", hardware_description.details.model);
+            Task::none()
+        }
+        USBEvent::DeviceLost(hardware_description) => {
+            println!("USB Device Lost: {}", hardware_description.details.model);
             Task::none()
         }
         USBEvent::Error(e) => Task::perform(empty(), move |_| Message::ConnectionError(e.clone())),
