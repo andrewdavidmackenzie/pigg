c5ca7a142b0fc32ffedecc5ef51bf8cdce640344
Andrew Mackenzie <andrew@mackenzie-serres.net>
2024-11-20 22:58:45 +0100

    Change comments

--- src/networking/mod.rs
+++ src/networking/mod.rs
@@ -1,16 +1,16 @@
 #[cfg(feature = "iroh")]
-#[allow(dead_code)] // TODO until we combine piglet and piggui versions
+#[allow(dead_code)] // Only used by piglet
 pub mod iroh_device;
-#[allow(dead_code)] // TODO until we combine piglet and piggui versions
+#[allow(dead_code)] // Not used by piglet
 #[cfg(feature = "iroh")]
 pub mod iroh_host;
 #[allow(dead_code)] // Not used by piglet
 pub mod local_device;
 #[cfg(feature = "tcp")]
-#[allow(dead_code)] // TODO until we combine piglet and piggui versions
+#[allow(dead_code)] // Only used by piglet
 pub mod tcp_device;
 #[cfg(feature = "tcp")]
-#[allow(dead_code)] // TODO until we combine piglet and piggui versions
+#[allow(dead_code)] // not ued by piglet
 pub mod tcp_host;
 
 #[cfg(feature = "iroh")]
