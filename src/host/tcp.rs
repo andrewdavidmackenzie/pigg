1de8046dcf1ef02b03f1519368cc566d35809e93
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2025-01-19 13:46:17 +0100

    Implement TcpConnection methods

--- src/host/tcp.rs
+++ src/host/tcp.rs
@@ -9,46 +9,51 @@ use std::net::IpAddr;
 use crate::hw_definition::config::HardwareConfigMessage::Disconnect;
 use crate::hw_definition::config::{HardwareConfig, HardwareConfigMessage};
 
-/// Wait until we receive a message from remote hardware over `stream`[TcpStream]
-pub async fn wait_for_remote_message(
-    mut stream: TcpStream,
-) -> Result<HardwareConfigMessage, anyhow::Error> {
-    let mut payload = vec![0u8; 1024];
-    let length = stream.read(&mut payload).await?;
-    ensure!(
-        length != 0,
-        io::Error::new(io::ErrorKind::BrokenPipe, "Connection closed")
-    );
-
-    Ok(postcard::from_bytes(&payload[0..length])?)
+#[derive(Clone)]
+pub struct TcpConnection {
+    stream: TcpStream,
 }
 
-/// Send config change received form the GUI to the remote hardware over `stream`[TcpStream]
-pub async fn send_config_message(
-    mut stream: TcpStream,
-    config_change_message: &HardwareConfigMessage,
-) -> anyhow::Result<()> {
-    stream
-        .write_all(&postcard::to_allocvec(&config_change_message)?)
-        .await?;
-    Ok(())
-}
+impl TcpConnection {
+    /// Connect to a remote piglet and get the initial message with the [HardwareDescription],
+    /// return that description plus the [TcpStream] to be used to communicate with it.
+    pub async fn connect(
+        ip: IpAddr,
+        port: u16,
+    ) -> anyhow::Result<(HardwareDescription, HardwareConfig, Self)> {
+        let mut stream = TcpStream::connect(format!("{ip}:{port}")).await?;
+        // This array needs to be big enough for HardwareDescription
+        let mut payload = vec![0u8; 1024];
+        let length = stream.read(&mut payload).await?;
+        let reply: (HardwareDescription, HardwareConfig) = postcard::from_bytes(&payload[0..length])?;
+        Ok((reply.0, reply.1, Self { stream }))
+    }
 
-/// Connect to a remote piglet and get the initial message with the [HardwareDescription],
-/// return that description plus the [TcpStream] to be used to communicate with it.
-pub async fn connect(
-    ip: IpAddr,
-    port: u16,
-) -> anyhow::Result<(HardwareDescription, HardwareConfig, TcpStream)> {
-    let mut stream = TcpStream::connect(format!("{ip}:{port}")).await?;
-    // This array needs to be big enough for HardwareDescription
-    let mut payload = vec![0u8; 1024];
-    let length = stream.read(&mut payload).await?;
-    let reply: (HardwareDescription, HardwareConfig) = postcard::from_bytes(&payload[0..length])?;
-    Ok((reply.0, reply.1, stream))
-}
+    /// Send config change received form the GUI to the remote hardware over `stream`[TcpStream]
+    pub async fn send_config_message(&mut self,
+                                     config_change_message: &HardwareConfigMessage,
+    ) -> anyhow::Result<()> {
+        self.stream
+            .write_all(&postcard::to_allocvec(&config_change_message)?)
+            .await?;
+        Ok(())
+    }
 
-/// Inform the device that we are disconnecting from TCP connection
-pub async fn disconnect(stream: TcpStream) -> anyhow::Result<()> {
-    send_config_message(stream, &Disconnect).await
+
+    /// Wait until we receive a message from remote hardware over `stream`[TcpStream]
+    pub async fn wait_for_remote_message(&mut self) -> Result<HardwareConfigMessage, anyhow::Error> {
+        let mut payload = vec![0u8; 1024];
+        let length = self.stream.read(&mut payload).await?;
+        ensure!(
+            length != 0,
+            io::Error::new(io::ErrorKind::BrokenPipe, "Connection closed")
+        );
+
+        Ok(postcard::from_bytes(&payload[0..length])?)
+    }
+
+    /// Inform the device that we are disconnecting from TCP connection
+    pub async fn disconnect(&mut self) -> anyhow::Result<()> {
+        self.send_config_message(&Disconnect).await
+    }
 }
