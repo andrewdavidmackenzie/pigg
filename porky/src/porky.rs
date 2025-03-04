e1de878e570381cd15d21e5a4cc886783d95b85d
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2025-03-04 13:06:24 +0100

    Fix pico builds

--- porky/src/porky.rs
+++ porky/src/porky.rs
@@ -20,6 +20,7 @@ use embassy_executor::Spawner;
 #[cfg(all(feature = "usb", feature = "wifi"))]
 use embassy_futures::select::{select, Either};
 use embassy_rp::bind_interrupts;
+#[cfg(feature = "pico2")]
 use embassy_rp::block::ImageDef;
 use embassy_rp::flash::{Blocking, Flash};
 #[cfg(feature = "wifi")]
@@ -81,6 +82,7 @@ mod mdns;
 /// The Pi Pico GPIO [PinDefinition]s that get passed to the GUI
 mod pin_descriptions;
 
+#[cfg(feature = "pico2")]
 #[link_section = ".start_block"]
 #[used]
 pub static IMAGE_DEF: ImageDef = ImageDef::secure_exe();
