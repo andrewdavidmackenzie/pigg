d248a0e780e2ec0aed3d64c561d8acda5165d0eb
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2025-03-29 09:14:52 +0100

    Ignore tcp tests on macos

--- piglet/tests/tcp.rs
+++ piglet/tests/tcp.rs
@@ -71,6 +71,7 @@ where
     connect_and_test(child, ip, port, test).await;
 }
 
+#[cfg_attr(target_os = "macos", ignore)]
 #[tokio::test]
 #[serial]
 async fn disconnect_tcp() {
@@ -86,6 +87,7 @@ async fn disconnect_tcp() {
     kill(&mut child)
 }
 
+#[cfg_attr(target_os = "macos", ignore)]
 #[tokio::test]
 #[serial]
 async fn config_change_returned_tcp() {
@@ -127,6 +129,7 @@ async fn config_change_returned_tcp() {
     kill(&mut child)
 }
 
+#[cfg_attr(target_os = "macos", ignore)]
 #[tokio::test]
 #[serial]
 async fn reconnect_tcp() {
