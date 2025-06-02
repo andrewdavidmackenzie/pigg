7ac57d19d68cfa9c322eb4d23843a5cd78a883b0
andrewdavidmackenzie <andrew@mackenzie-serres.net>
2025-06-02 09:34:24 +0200

    Try and fix IP test

--- piggui/tests/tcp.rs
+++ piggui/tests/tcp.rs
@@ -1,16 +1,22 @@
-use crate::support::parse_pigglet;
+use crate::support::{build, kill_all, parse_pigglet};
 use serial_test::serial;
+use std::time::Duration;
 use support::{pass, run, wait_for_stdout};
 
 mod support;
 
-// TODO fix networking issue in ubuntu and macos in GH Actions
 #[cfg(feature = "tcp")]
 #[tokio::test]
 #[serial]
 async fn connect_via_ip() {
+    kill_all("pigglet");
+    build("pigglet");
     let mut pigglet = run("pigglet", vec![], None);
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
     let (ip, port, _) = parse_pigglet(&mut pigglet).await;
+
     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
 
     let mut piggui = run(
