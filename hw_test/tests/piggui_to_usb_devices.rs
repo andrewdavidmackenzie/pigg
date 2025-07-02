a90e9ba0b7986e272b80ba28bf7404e309c547a1
Andrew Mackenzie <andrew@mackenzie-serres.net>
2025-07-02 19:18:14 +0200

    Add a failing test to see if can connect using partial USB serial number

--- hw_test/tests/piggui_to_usb_devices.rs
+++ hw_test/tests/piggui_to_usb_devices.rs
@@ -33,4 +33,31 @@ async fn usb_discover_and_connect_usb() {
     );
 }
 
+/// Test that if a partial serial number is passed it also works
+#[tokio::test]
+#[serial]
+async fn usb_discover_and_connect_partial_usb() {
+    let serials = usb_host::get_serials()
+        .await
+        .expect("No usb porky attached");
+
+    let number = serials.len();
+    assert!(number > 0, "Could not find by USB to connect to by USB");
+
+    for serial in serials {
+        let partial_serial = serial[..serial.len() / 2].to_string();
+        let mut piggui = run("piggui", vec!["--usb".to_string(), partial_serial], None);
+
+        wait_for_stdout(&mut piggui, "Connected to hardware")
+            .expect("Did not get connected message using partial USB serial number");
+
+        kill(&mut piggui);
+    }
+
+    println!(
+        "Tested piggui USB connection to {} USB discovered devices using partial USB serial number",
+        number
+    );
+}
+
 //reconnect usb (kill and restart)
