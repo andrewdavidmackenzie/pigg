370da35bc85aa0aeff4e5f7919d79de5847119bb
Andrew Mackenzie <andrew@mackenzie-serres.net>
2025-07-31 10:26:09 +0200

    Don't output endpoint- verbose and not useful

--- pigglet/src/device_net/iroh_device.rs
+++ pigglet/src/device_net/iroh_device.rs
@@ -43,7 +43,6 @@ impl Display for IrohDevice {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         writeln!(f, "nodeid:{}", self.nodeid)?;
         writeln!(f, "relay URL:{}", self.relay_url)?;
-        writeln!(f, "Endpoint:{:?}", self.endpoint)?;
         Ok(())
     }
 }
